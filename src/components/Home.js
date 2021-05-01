204928b39d8a88fce97354ec8a50f0d37f250227
Nazariy995 <nazariy1995@gmail.com>
2021-04-30 22:51:38 -0700

    ADD: viewers

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import styled from 'styled-components'
+import ImgSlider from './ImgSlider'
+import Viewers from './Viewers'
 
 function Home() {
     return (
-        <div>
-            Home
-        </div>
+        <Container>
+            <ImgSlider />
+            <Viewers />
+        </Container>
     )
 }
 
 export default Home
+
+const Container = styled.main`
+    padding: 0 calc(3.5vw + 5px);
+    height: 100vh;
+    overflow-x: hidden;
+    &:before {
+        background: url("/images/home-background.png") center center / cover
+        no-repeat fixed;
+        content: "";
+        position: absolute;
+        top: 0; 
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: -1;
+    }
+`
+
+
